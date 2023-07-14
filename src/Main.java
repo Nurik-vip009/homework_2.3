3b62da9b68859417cec19702d236f82012f69030
Nurik-vip009 <raimkulovnurislam095@gmail.com>
2023-07-14 15:57:19 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,12 @@ public class Main {
         } catch (LimitException e) {
             System.out.println("Произошло исключение: " + e.getMessage());
             System.out.println("Снято только: " + e.getRemainingAmount() + " сом");
+
+            try {
+                account.withdraw(e.getRemainingAmount());
+            } catch (LimitException ex) {
+                System.out.println("Произошло исключение при снятии остатка: " + ex.getMessage());
+            }
         }
     }
 }
